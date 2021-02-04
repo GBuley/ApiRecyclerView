bb73f426a24bb8c9c88e7ff2e229938a3b3d7223
BuleyG <gtbuley@gmail.com>
2021-02-04 00:03:36 -0600

    Removed a ton of unused imports

--- app/src/main/java/grant/com/apiguy/view/MainActivity.java
+++ app/src/main/java/grant/com/apiguy/view/MainActivity.java
@@ -1,48 +1,23 @@
 package grant.com.apiguy.view;
 
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Network;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.android.material.button.MaterialButton;
 import com.google.android.material.imageview.ShapeableImageView;
-import com.google.android.material.textview.MaterialTextView;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
-import Repo.ShibeRepository;
-import grant.com.apiguy.R;
 import grant.com.apiguy.adapter.ShibeAdapter;
 import grant.com.apiguy.databinding.ActivityMainBinding;
 import grant.com.apiguy.viewModel.MainViewModel;
-import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.moshi.MoshiConverterFactory;
-import retrofit2.http.GET;
 
 public class MainActivity extends AppCompatActivity {
     private MainViewModel viewModel;
